6edbaf1454759c02b03ea16a488099538257e725
wizardofgit <119754054+wizardofgit@users.noreply.github.com>
2022-12-11 21:17:47 +0100

    zmiana w main.py

--- traffic/car.py
+++ traffic/car.py
@@ -1,9 +1,10 @@
 class Car:
-    def __init__(self, id, acceleration, length, direction):
-        self.id = id #id auta
-        self.acceleration = acceleration #przyspieszenie w km/s
-        self.length = length #dlugosc auta
-        self.direction = direction #kierunek w jaki jedzie auto
+    def __init__(self, id, acceleration, length, direction, max_v):
+        self.id = id #car id
+        self.acceleration = acceleration #acceleration in km/s
+        self.length = length #length of car in m
+        self.direction = direction #direction in which car is going
+        self.max_v = max_v #max velocity of the car
 
     def __str__(self):
-        return f"Car with id {self.id} with acceleration {self.acceleration}, length {self.length} going {self.direction}"
+        return f"Car id={self.id} with acceleration {self.acceleration}, length {self.length} going {self.direction}"
