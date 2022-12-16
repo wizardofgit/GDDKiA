08b1016988b3bd1de60b60c940ec17124daf74c8
wizardofgit <119754054+wizardofgit@users.noreply.github.com>
2022-12-16 03:15:13 +0100

    naprawa praktycznie wszystkich dotychczasowych błędów

--- main.py
+++ main.py
@@ -2,51 +2,54 @@ from traffic.car import Car
 import pygame
 import random
 from sys import exit
+import json
 
 dt = 0.01 #step of the simulation in seconds
 width, height = 1280, 720 #width and height of the window
-starting_positions_cord = {'north': [width/2 - 5, 5],
-                           'south': [width/2 + 5, height],
-                           'east': [width, height/2 - 5],
-                           'west': [5, height/2 + 5]}  #starting coordinates for different directions
+starting_positions_cord = {'north': (width/2 - 5, 0),
+                           'south': (width/2 + 5, height),
+                           'east': (width, height/2 - 5),
+                           'west': (0, height/2 + 5)}  #starting coordinates for different directions
 car_queues = {'east_right': [], 'east_left': [],
               'west_right': [], 'west_left': [],
               'south_down': [], 'south_up': [],
               'north_down': [], 'north_up': []} #contains the ids of cars that are in a certain segment of the road
+status_log = {} #status of all vehicles
 cars = [] #list of existing cars (i.e. being displayed)
 current_free_car_id = 0 #current car id that can be used to generate a new car
 cars_passed = 0 #how many cars has entered and cleared the junction
 time_elapsed = 0.0 #how much time has passed since the start of the simulation
 time_threshold = 0 #after certain threshold new cars will be generated
 time_threshold_step = 2 #time_threshold will be updated, so that cars are generated every time_threshold_step seconds
+minimum_cars_passed = 10 #number of cars which passed after which the simulation ends
+t, t_step = 0, 0.01 #variables controlling how often info is logged
 
 def generate_traffic(direction = None, selected_starting_point = None): #randomly generates a car
     id = current_free_car_id
-    acceleration = random.randint(14, 18) * 0.5
+    #acceleration = random.randint(14, 18) * 0.5
+    acceleration = 9
     length = round(random.randint(40, 55) * 0.1, 1)
     if direction == None:
         direction = ['ahead', 'left', 'ahead','right', 'ahead', 'ahead'][random.randint(0,5)]
-    max_v = float(random.randint(100, 160))
+    #max_v = float(random.randint(100, 160))
+    max_v = 140
     if selected_starting_point == None:
-        selected_starting_point = random.randint(0, 3)
-    starting_position = [list(starting_positions_cord.keys())[selected_starting_point],
-                         starting_positions_cord[list(starting_positions_cord.keys())[selected_starting_point]]] #include list consisting of starting position and
-                                                                                                                 #its coordinates
-
+        selected_starting_point = list(starting_positions_cord.keys())[random.randint(0, 3)]
+    starting_position = [selected_starting_point, starting_positions_cord[selected_starting_point]] #include list consisting of starting position and its coordinates
+    print(starting_positions_cord)
+    print(starting_position)
     cars.append(Car(id, acceleration, length, direction, max_v, starting_position, dt, height, width))
 
-def show_car_info(id = None, start_printing = 1.00, step = 2.00): #prints info about all cars or car with certain id
-    if id is None:
-        if time_elapsed > start_printing:
-            for car in cars:
-                print(car, time_elapsed)
-            start_printing += step
-    else:
-        if time_elapsed > start_printing:
-            for car in cars:
-                if car.id == id:
-                    print(car)
-            start_printing += step
+def show_car_info(): #prints info about all cars or car with certain id
+    for car in cars:
+        print("Time elapsed: ",time_elapsed)
+        if 'car_' + str(car.id) in status_log:
+            status_log['car_' + str(car.id)].append(car.__str__())
+        else:
+            status_log['car_' + str(car.id)] = []
+            status_log['car_' + str(car.id)].append(car.__str__())
+        print(car)
+    print('')
 
 #create simple plain color road and car images
 road_horizontal = pygame.Surface((width, 20))
@@ -68,18 +71,22 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Symulacja skrzyżowania")
 clock = pygame.time.Clock()
 text = pygame.font.Font(None, 50)
-
-generate_traffic()
+car_id_text = pygame.font.Font(None, 20)
+car_on_screen_text = pygame.font.Font(None, 50)
 
 while True:
     for event in pygame.event.get():    #check what user did
         if event.type == pygame.QUIT:   #if user clicked 'x' (aka quit) in window
             pygame.quit()
             print("Average car flow: ", cars_passed / time_elapsed * 3600, " per hour")
+            with open('status_log.json', 'w') as f: #saves status log as a json file
+                f.write(json.dumps(status_log))
             exit()  #stop the app
-    if len(cars) == 0 and time_elapsed > 5:
+    if cars_passed >= minimum_cars_passed:
         pygame.quit()
         print("Average car flow: ", cars_passed / time_elapsed * 3600, " per hour")
+        with open('status_log.json', 'w') as f: #saves status log as a json file
+            f.write(json.dumps(status_log))
         exit()  # stop the app
 
     #update images on screen
@@ -89,6 +96,8 @@ while True:
     screen.blit(road_vertical, (width/2 - 5, 0))
     text_image = text.render(str(round(time_elapsed, 2)), True, 'White')
     screen.blit(text_image, (0,0))
+    car_on_screen_text_image = car_on_screen_text.render(str(len(cars)), True, 'White')
+    screen.blit(car_on_screen_text_image, (0,30))
 
     #generates another car after certain in-simulation seconds elapsed
     if time_elapsed > time_threshold:
@@ -96,11 +105,13 @@ while True:
         current_free_car_id += 1
         time_threshold += time_threshold_step
 
-    #update the parameters of every car on the road; check if cars are to be deleted
+    if time_elapsed > t:
+        t += t_step
+        show_car_info()
+
+    # update the parameters of every car on the road; check if cars are to be deleted
     cars_to_be_deleted = []
-    show_car_info()
-    for i in range(len(cars)):
-        car = cars[i]
+    for car in cars:
         car.update()
 
         if car.reached_destination:
@@ -114,10 +125,14 @@ while True:
     #display all the cars
     for car in cars:
         if car.car_orientation == 1:
-            screen.blit(car_image_horizontal, tuple(car.current_position))
+            id_image = car_id_text.render(str(car.id), True, 'Black')
+            screen.blit(car_image_horizontal, car.current_position)
+            screen.blit(id_image, car.current_position)
         else:
-            screen.blit(car_image_vertical, tuple(car.current_position))
+            screen.blit(car_image_vertical, car.current_position)
+            id_image = car_id_text.render(str(car.id), True, 'Black')
+            screen.blit(id_image, car.current_position)
 
     time_elapsed += dt
     pygame.display.update() #update the screen
-    clock.tick(180) #simulation should run at no more than said amount of ticks per second
+    clock.tick(60) #simulation should run at no more than said amount of ticks per second
